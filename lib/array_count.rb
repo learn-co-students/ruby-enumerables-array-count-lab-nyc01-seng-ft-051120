881d64573b3a883a56390b0baef0621d5080de98
ultimate-scope-5575 <a.bystranowska.takahashi@gmail.com>
2020-04-28 19:52:07 +0000

    Done.

--- lib/array_count.rb
+++ lib/array_count.rb
@@ -1,7 +1,25 @@
 def count_strings(array)
   # Return the total number of strings in the provided array using the count enumerable
+  counter = 0;
+  strings = [];
+  while counter < array.length do
+  if  array[counter].class == String
+   strings.push(array[counter])
+  end
+  counter += 1;
+end
+return strings.count
 end
 
 def count_empty_strings(array)
   # Return the total number of EMPTY strings in the provided array using the count enumerable
+      counter = 0;
+  strings = [];
+  while counter < array.length do
+  if  array[counter].class == String && array[counter].length == 0
+   strings.push(array[counter])
+  end
+  counter += 1;
+end
+return strings.count
 end
